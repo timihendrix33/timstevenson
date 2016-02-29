8b6972dd5c49bc74fe61241047f500515252f9f3
tstevenson <t.stevenson33@gmail.com>
2016-02-28 20:53:06 -0500

    trying express with no routes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// var express = require('express');
-// var app     = express();
+var express = require('express');
+var app     = express();
 
-// app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
-// //For avoidong Heroku $PORT error
+//For avoidong Heroku $PORT error
 // app.get('/', function(request, response) {
 //     var result = 'App is running'
 //     response.send(result);
@@ -11,4 +11,4 @@
 //     console.log('App is running, server is listening on port ', app.get('port'));
 // });
 
-var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
+//var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
