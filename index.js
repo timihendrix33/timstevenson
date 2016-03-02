2c58e91fcef0f5c18b4953d1144c42792e7d2523
tstevenson <t.stevenson33@gmail.com>
2016-03-01 19:41:21 -0500

    think I'm onto something

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('/');
+  response.render('index.html');
 });
 
 app.listen(app.get('port'), function() {
